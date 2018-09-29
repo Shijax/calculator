e692dc94ae903edb350f954ceb305a415101a4d4
Matthew Jackson <mattshiloh@gmail.com>
2018-09-29 11:46:38 -0500

    before lunch merge

---
+++ javascripts/helpers/maths.js
@@ -0,0 +1,22 @@
+const multiply = (n1, n2) => {
+    return n1 * n2;
+};
+
+const divide = (n1, n2) => {
+    return n1 / n2;
+};
+
+const add = (n1, n2) => {
+    return n1 + n2;
+};
+
+const subtract = (n1, n2) => {
+    return n1 - n2;
+};
+
+export {
+    multiply,
+    divide,
+    add,
+    subtract
+}
